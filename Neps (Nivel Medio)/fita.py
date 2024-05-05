53b033f4665aecd386cb8363fe6bdc33d42c0470
Eric Barbosa <ericbarbosa4050@gmail.com>
2024-05-05 19:18:57 -0300

    Consertando código (nota baixa no Pratique)

--- Neps (Nivel Medio)/fita.py
+++ Neps (Nivel Medio)/fita.py
@@ -1,20 +1,5 @@
-n = int(input())
-lista = list(map(int, input().split()))
-listaColorida, indexes = [], []
-
-aux = lista.copy()
-
-while 0 in aux:
-    indexes.append(aux.index(0))
-    aux[aux.index(0)] = "_"
-
-for i in range(len(lista)):
-    if lista[i] == -1:
-        distancias = []
-        for index in indexes:
-            distancias.append(abs(i-index))
-        listaColorida.append(min(distancias))
-    else:
-        listaColorida.append(0)
-
-print(" ".join([str(i) for i in listaColorida]))
+n, lista = int(input()), input().split()
+indexes = [index for index, num in enumerate(lista) if num == "0"]
+listaColorida = [min(abs(index-indexZero) if abs(index-indexZero) < 9 else 9 for indexZero in indexes) for index in range(n)]
+for i in listaColorida:
+    print(i, end=" ")
