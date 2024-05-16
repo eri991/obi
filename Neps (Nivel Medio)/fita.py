e8297d88ec8df54108fcf87f0e696b6d88e80360
Eric Barbosa <ericbarbosa4050@gmail.com>
2024-05-16 16:44:01 -0300

    Reduzindo linha de código fita.py

--- Neps (Nivel Medio)/fita.py
+++ Neps (Nivel Medio)/fita.py
@@ -1,5 +1,4 @@
 n, lista = int(input()), input().split()
 indexes = [index for index, num in enumerate(lista) if num == "0"]
 listaColorida = [min(abs(index-indexZero) if abs(index-indexZero) < 9 else 9 for indexZero in indexes) for index in range(n)]
-for i in listaColorida:
-    print(i, end=" ")
+print(*listaColorida)
