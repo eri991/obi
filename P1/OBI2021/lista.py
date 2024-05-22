e8f00d91ecae72cbaa926679ca1b630fa6b0d075
Eric Barbosa <ericbarbosa4050@gmail.com>
2024-05-22 20:43:21 -0300

    Melhorando tempo de execução

--- P1/OBI2021/lista.py
+++ P1/OBI2021/lista.py
@@ -1,18 +1,16 @@
 n, lista = int(input()), list(map(int, input().split()))
 ops = i = 0
-while True:
-    if lista == lista[::-1]:
-        print(ops)
-        break
-    else:
-        if lista[i] < lista[len(lista)-1-i]:
-            lista[i] += lista[i+1]
-            lista.remove(lista[i+1])
-            ops += 1
-            i -= 1
-        elif lista[len(lista)-1-i] < lista[i]:
-            lista[len(lista)-1-i] += lista[len(lista)-2-i]
-            lista.remove(lista[len(lista)-2-i])
-            ops += 1
-            i -= 1
+while lista != lista[::-1]:
+    if lista[i] < lista[n-1-i]:
+        lista[i] += lista[i+1]
+        lista.remove(lista[i+1])
+        ops += 1
+        i -= 1
+    elif lista[n-1-i] < lista[i]:
+        lista[n-1-i] += lista[n-2-i]
+        lista.remove(lista[n-2-i])
+        ops += 1
+        i -= 1
     i += 1
+
+print(ops)
